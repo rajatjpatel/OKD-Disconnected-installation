42493493807223fd23bd6e92d528dc9363630ba3
rajatjpatel <rajatjpatel@gmail.com>
2019-08-05 15:24:43 +0400

    Update node101.sh

--- node101.lab.local/node101.sh
+++ node101.lab.local/node101.sh
@@ -1,21 +1,21 @@
-hostnamectl set-hostname node101.lab.local
-sed -i "\$ a $1 192.168.56.100\t\t$NEW_HOST foreman.lab.local\t$NEW_HOST" /etc/hosts
-sed -i "\$ a $1 192.168.56.101\t\t$NEW_HOST node101.lab.local\t$NEW_HOST" /etc/hosts
-sed -i "\$ a $1 192.168.56.102\t\t$NEW_HOST node102.lab.local\t$NEW_HOST" /etc/hosts
-sed -i "\$ a $1 192.168.56.103\t\t$NEW_HOST node103.lab.local\t$NEW_HOST" /etc/hosts
-sed -i "\$ a $1 192.168.56.104\t\t$NEW_HOST node104.lab.local\t$NEW_HOST" /etc/hosts
-mkdir -p /etc/docker/certs.d/foreman.lab.local:5000
-curl http://foreman.lab.local/pub/katello-server-ca.crt > /etc/docker/certs.d/foreman.lab.local\:5000/katello-server-ca.crt
-scp root@foreman.lab.local:/etc/pki/tls/certs/localhost.crt /etc/docker/certs.d/foreman.lab.local\:5000/ca.crt  
-systemctl restart docker
+hostnamectl set-hostname node101.lab.local && echo "Step Finish" &&
+sed -i "\$ a $1 192.168.56.100\t\t$NEW_HOST foreman.lab.local\t$NEW_HOST" /etc/hosts && echo "Step Finish" &&
+sed -i "\$ a $1 192.168.56.101\t\t$NEW_HOST node101.lab.local\t$NEW_HOST" /etc/hosts && echo "Step Finish" &&
+sed -i "\$ a $1 192.168.56.102\t\t$NEW_HOST node102.lab.local\t$NEW_HOST" /etc/hosts && echo "Step Finish" &&
+sed -i "\$ a $1 192.168.56.103\t\t$NEW_HOST node103.lab.local\t$NEW_HOST" /etc/hosts && echo "Step Finish" &&
+sed -i "\$ a $1 192.168.56.104\t\t$NEW_HOST node104.lab.local\t$NEW_HOST" /etc/hosts && echo "Step Finish" &&
+mkdir -p /etc/docker/certs.d/foreman.lab.local:5000 && echo "Step Finish" &&
+curl http://foreman.lab.local/pub/katello-server-ca.crt > /etc/docker/certs.d/foreman.lab.local\:5000/katello-server-ca.crt && echo "Step Finish" &&
+scp root@foreman.lab.local:/etc/pki/tls/certs/localhost.crt /etc/docker/certs.d/foreman.lab.local\:5000/ca.crt   && echo "Step Finish" &&
+systemctl restart docker && echo "Step Finish" &&
 
-yum update -y
-yum install -y wget git  nano net-tools docker-1.13.1 bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct openssl-devel httpd-tools NetworkManager python-cryptography python-devel python-passlib java-1.8.0-openjdk-headless "@Development Tools"
-yum -y install centos-release-openshift-origin311 epel-release docker git pyOpenSSL
-systemctl start NetworkManager
-systemctl enable NetworkManager
-systemctl status NetworkManager
-ssh-keygen -f ~/.ssh/id_rsa -N '' 
+yum update -y && echo "Step Finish" &&
+yum install -y wget git  nano net-tools docker-1.13.1 bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct openssl-devel httpd-tools NetworkManager python-cryptography python-devel python-passlib java-1.8.0-openjdk-headless "@Development Tools" && echo "Step Finish" &&
+yum -y install centos-release-openshift-origin311 epel-release docker git pyOpenSSL && echo "Step Finish" &&
+systemctl start NetworkManager && echo "Step Finish" &&
+systemctl enable NetworkManager && echo "Step Finish" &&
+systemctl status NetworkManager && echo "Step Finish" &&
+ssh-keygen -f ~/.ssh/id_rsa -N ''  && echo "Step Finish" &&
  cat <<EOF >~/.ssh/config
  Host node101
     Hostname node101.lab.local
@@ -26,12 +26,12 @@ Host node102
 Host node103
     Hostname node103.lab.local
     User root
-EOF
-chmod 600 ~/.ssh/config
+EOF && echo "Step Finish" &&
+chmod 600 ~/.ssh/config && echo "Step Finish" &&
 
-for host in node101.lab.local node102.lab.local node103.lab.local ; do ssh-copy-id -i ~/.ssh/id_rsa.pub $host; done
+for host in node101.lab.local node102.lab.local node103.lab.local ; do ssh-copy-id -i ~/.ssh/id_rsa.pub $host; done && echo "Step Finish" &&
 
-yum -y install openshift-ansible
+yum -y install openshift-ansible && echo "Step Finish" &&
 
 cat <<EOF > ~/inentory.ini
 # Create an OSEv3 group that contains the masters, nodes, and etcd groups
@@ -68,6 +68,6 @@ node102.lab.local openshift_node_group_name='node-config-compute'
 node103.lab.local openshift_node_group_name='node-config-compute'
 node104.lab.local openshift_node_group_name='node-config-infra'
 
-EOF
-ansible-playbook -i inentory.ini /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml 
+EOF && echo "Step Finish" &&
+ansible-playbook -i inentory.ini /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml && echo "Step Finish" &&
 ansible-playbook -i inentory.ini /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml 
